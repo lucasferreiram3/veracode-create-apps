e2578a6885aa6d3be6e112104c8240056979215f
lucasferreiram3 <lucas.ferreira@m3corp.com.br>
2024-07-16 13:53:09 -0300

    add json payload

--- apps.py
+++ apps.py
@@ -8,39 +8,126 @@ import json
 import sys
 
 # ---------------------------------------------- VARIAVEIS  ---------------------------------------------- #
-appName = "verademo-dotnetcore"
+
 api_base = "https://api.veracode.com/appsec/v1"
 headers = {"User-Agent": "Python HMAC Example"}
-policy = ''
-app_profiles = []
-guid_profiles = []
-default_policy = ['Veracode Recommended Low', 'Veracode Recomended Medium', 'Veracode Recomended High']
-#teams_default = ["DevSecOps", "CDE Projetos e Arquitetura de Segurança", "API-Services", "Auditoria Interna", "Cyber Security"]
-business_unit = 'M3_PreVendas'
-teams_default = ['Showroom_Lucas', 'Showroom Pré-Vendas']
+appName = "verademo-dotnetcore"
+criticality = ''
+business_unit = 'M3_Prevendas'
+owner = 'example@exe.com.br'
+custom_fields = []
 squad = ''
 lider = ''
 po = ''
-possui_api = ['yes', 'no']
-exposed = ['yes', 'no']
-owner = 'example@exe.com.br'
-custom_fields = []
+possui_api = ''
+exposed = ''
+description = ''
+policy = ''
+teams = ''
+app_profiles = []
+guid_profiles = []
+default_policy = 'Veracode Recommended Low'
+teams_default = ['Showroom_Lucas', 'Showroom Pré-Vendas']
 
+#default_policy = 'CIP Medium + SCA'
+#teams_default = ["DevSecOps", "CDE Projetos e Arquitetura de Segurança", "API-Services", "Auditoria Interna", "Cyber Security"]
+#business_unit = 'SAP'
+
+# ---------------------------------------------- PAYLOAD  ---------------------------------------------- #
+
+payload = {
+    "guid": "string",
+    "id": 0,
+    "oid": 0,
+    "organization_id": 0,
+    "profile": {
+      "name": f"{appName}",
+      "business_criticality": f"{criticality}",
+      "business_owners": [
+        {
+          "email": f"{owner}",
+          "name": f"{owner}"
+        }
+      ],
+      "business_unit": {
+        "guid": f"{business_unit}"
+      },
+      "custom_field_values": [
+        {
+          "app_custom_field_name": {
+            "name": "custom_field",
+            "organization_id": 0,
+            "sort_order": 0
+          },
+          "field_name_id": 0,
+          "id": 0,
+          "value": "string"
+        }
+      ],
+      "custom_fields": [
+        {
+          "name": "Squad Responsável",
+          "value": f"{squad}"
+        },
+        {
+            "name": "Líder Técnico",
+            "value": f"{lider}"
+        },
+        {
+            "name": "Product Owner",
+            "value": f"{po}"
+        },
+        {
+            "name": "Possui API? (Sim ou Não)",
+            "value": f"{possui_api}"
+        },
+        {
+            "name": "Sofre exposição à internet” (Sim ou Não)",
+            "value": f"{exposed}"
+        }
+      ],
+      "description": f"{description}",
+      "name": "string",
+      "policies": [
+        {
+          "guid": f"{policy}",
+          "is_default": "true"
+        }
+      ],
+      "settings": {
+        "dynamic_scan_approval_not_required": "false",
+        "nextday_consultation_allowed": "true",
+        "sca_enabled": "true",
+        "static_scan_dependencies_allowed": "true"
+      },
+      "tags": "string",
+      "teams": [
+        {
+          "guid": f"{teams}"
+        }
+      ]
+    },
+    "scans": [
+      {
+        "internal_status": "string",
+        "status": "CREATED"
+      }
+    ]
+}
 # ---------------------------------------------- FUNCOES  ---------------------------------------------- #
 
 def main():
-    #getPolicyGuid('Veracode Recommended Low')
-    #getBuGuid(business_unit)
-    #getTeamsGuid('Showroom_Lucas')
-    checkAppProfile(appName)
-    #print(getAppGuid(appName))
+
+    #checkAppProfile(appName)
+    print(getAppGuid(appName))
+    print(getPolicyGuid(default_policy))
+    print(getBuGuid(business_unit))
 
 # Retorna o GUID da política
 def getPolicyGuid(p):
-    data = Policies.get_all(policy)
+    data = Policies.get_all(p)
     for i in data:
-        if i["name"] == "Veracode Recommended Low":
-            #print(f'{i["name"]} : {i["guid"]}')
+        if i["name"] == p:
             return i["guid"]
 
 # Retorna o GUID da Business Unit
@@ -48,7 +135,6 @@ def getBuGuid(b):
     data = BusinessUnits().get_all()
     for i in data:
         if i["bu_name"] == "M3_PreVendas":
-            #print(f'{i["bu_name"]} : {i["bu_id"]}')
             return i["bu_id"]
 
 # Retorna o GUID dos times
@@ -56,7 +142,6 @@ def getTeamsGuid(t):
     data = Teams().get_all(teams_default)
     for i in data:
         if i["team_name"] in teams_default:
-            #print(f'{i["team_name"]} : {i["team_id"]}')
             return i["team_id"]
         
 # Retorna o perfil de aplicação já existe na plataforma      
@@ -99,6 +184,7 @@ def getAppGuid(a):
            return app["profile"]["business_unit"]["guid"]
     else:
         print(response.status_code)
+
 
 if __name__ == "__main__":
 
